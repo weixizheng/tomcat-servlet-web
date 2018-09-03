a582b1a99728b655b1ef4ccb9e7ade7d03797c18
nick <nick@eventslack.com>
2018-09-03 15:40:26 +0800

    jdbc CURD简单封装测试

---
+++ src/com/nick/bean/MyUser.java
@@ -0,0 +1,42 @@
+package com.nick.bean;
+
+import java.util.Date;
+
+public class MyUser {
+    private int userId;
+    private String userName;
+    private int type;
+    private Date createTime;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ src/com/nick/dao/login/LoginDAO.java
@@ -0,0 +1,12 @@
+package com.nick.dao.login;
+
+import com.nick.bean.MyUser;
+import com.nick.dao.common.BaseDAO;
+
+public class LoginDAO extends BaseDAO<MyUser> {
+
+
+    public static void main(String[] args) {
+        BaseDAO loginDAO = new LoginDAO();
+    }
+}
